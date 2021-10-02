ba3fb661180586d5d71e8a953c9dfbb0f13784a6
biko-73 <57875905+biko-73@users.noreply.github.com>
2021-10-02 23:10:03 +0200

    V 3.26
    Amended Dreambox infobar framerate code
    
    Fixed typo in infobar preventing icons displaying
    
    Put in another catch for dodgy providers bringing back crap and invalid code.

--- installer.sh
+++ installer.sh
@@ -12,8 +12,8 @@
 
 PACKAGE_DIR='xstreamity/main'
 
-MY_IPK="enigma2-plugin-extensions-xstreamity_3.25_all.ipk"
-MY_DEB="eenigma2-plugin-extensions-xstreamity_3.25_all.deb"
+MY_IPK="enigma2-plugin-extensions-xstreamity_3.26_all.ipk"
+MY_DEB="eenigma2-plugin-extensions-xstreamity_3.26_all.deb"
 
 ########################################################################################################################
 # Auto ... Do not change
