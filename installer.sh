292bc078f570cc12232e611d9ea21ae55088cac1
biko-73 <57875905+biko-73@users.noreply.github.com>
2022-11-25 21:02:08 +0200

    Fix sleep on OBH

--- installer.sh
+++ installer.sh
@@ -75,7 +75,9 @@ if [ -f $MY_TMP_FILE ]; then
 		if which systemctl > /dev/null 2>&1; then
 			sleep 2; systemctl restart enigma2
 		else
-			init 4; sleep 4; init 3;
+			init 4
+			sleep 4 > /dev/null 2>&1
+			init 3
 		fi
 	else
 		echo "   >>>>   INSTALLATION FAILED !   <<<<"
