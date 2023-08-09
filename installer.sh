56fba95232d3a63b8d7a1589ae065ee49ed5500b
biko-73 <57875905+biko-73@users.noreply.github.com>
2023-08-10 00:43:08 +0300

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -12,8 +12,8 @@
 
 PACKAGE_DIR='xstreamity/main'
 
-MY_IPK="enigma2-plugin-extensions-xstreamity_4.04_all.ipk"
-MY_DEB="enigma2-plugin-extensions-xstreamity_4.04_all.deb"
+MY_IPK="enigma2-plugin-extensions-xstreamity_4.05_all.ipk"
+MY_DEB="enigma2-plugin-extensions-xstreamity_4.05_all.deb"
 
 ########################################################################################################################
 # Auto ... Do not change
