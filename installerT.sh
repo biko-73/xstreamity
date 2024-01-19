65845f06bb60dbb80c50414fdbcf34012332cbaa
biko-73 <57875905+biko-73@users.noreply.github.com>
2024-01-19 14:46:00 +0200

    Update installerT.sh

--- installerT.sh
+++ installerT.sh
@@ -12,8 +12,8 @@
 
 PACKAGE_DIR='xstreamity/main'
 
-MY_IPK="enigma2-plugin-extensions-xstreamity-toys_skin_2.00_all.ipk"
-MY_DEB="enigma2-plugin-extensions-xstreamity-toys_skin_2.00_all.deb"
+MY_IPK="enigma2-plugin-extensions-xstreamity-toys-skin_2.00_all.ipk"
+MY_DEB="enigma2-plugin-extensions-xstreamity-toys-skin_2.00_all.deb"
 
 ########################################################################################################################
 # Auto ... Do not change
