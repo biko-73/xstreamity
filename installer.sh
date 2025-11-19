9a5a0ef34226d1e43ba8e9bc128c21159f72eed1
biko-73 <57875905+biko-73@users.noreply.github.com>
2025-11-20 01:13:56 +0300

    Update package versions to 5.20

--- installer.sh
+++ installer.sh
@@ -12,8 +12,8 @@
 
 PACKAGE_DIR='xstreamity/main'
 
-MY_IPK="enigma2-plugin-extensions-xstreamity_5.19_all.ipk"
-MY_DEB="enigma2-plugin-extensions-xstreamity_5.19_all.deb"
+MY_IPK="enigma2-plugin-extensions-xstreamity_5.20_all.ipk"
+MY_DEB="enigma2-plugin-extensions-xstreamity_5.20_all.deb"
 
 ########################################################################################################################
 # Auto ... Do not change
