19329ab6eac4480388ac8a870f3b6002c12a0b9a
biko-73 <57875905+biko-73@users.noreply.github.com>
2026-03-30 09:59:17 +0200

    Update package version to 5.38

--- installer.sh
+++ installer.sh
@@ -12,8 +12,8 @@
 
 PACKAGE_DIR='xstreamity/main'
 
-MY_IPK="enigma2-plugin-extensions-xstreamity_5.37_all.ipk"
-MY_DEB="enigma2-plugin-extensions-xstreamity_5.37_all.deb"
+MY_IPK="enigma2-plugin-extensions-xstreamity_5.38_all.ipk"
+MY_DEB="enigma2-plugin-extensions-xstreamity_5.38_all.deb"
 
 ########################################################################################################################
 # Auto ... Do not change
